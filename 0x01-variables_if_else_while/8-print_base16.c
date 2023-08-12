3abff00e670bf71213a74a8d094b33fff47d567e
M-Abdulwaheed <muhd.abdulwaheed@gmail.com>
2023-08-12 08:35:00 -0700

    Hexa

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -11,9 +11,9 @@ int main(void)
 	int i;
 	char j;
 
-	for (i = '0' ; i <= '9' ; i++)
-		putchar(i + 'o');
-	for (j = 'a' ; j <= 'f' ; i++)
+	for (i = 0 ; i <= 9 ; i++)
+		putchar(i + '0');
+	for (j = 'a' ; j <= 'f' ; j++)
 		putchar(j);
 	putchar('\n');
 	return (0);
