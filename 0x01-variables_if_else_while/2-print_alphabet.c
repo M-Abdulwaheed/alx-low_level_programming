06360659f3e64ff451e277f428b7baa459008c4a
M-Abdulwaheed <muhd.abdulwaheed@gmail.com>
2023-08-11 20:42:16 -0700

    Sometimes

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * return: alway return 0
+ *
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 'a'; i < 'z' ; i++)
+		putchar (i);
+	putchar('\n');
+	return (0);
+}
