21b5d6359c9200137f6ef190668974d690318c03
M-Abdulwaheed <muhd.abdulwaheed@gmail.com>
2023-08-11 21:12:40 -0700

    aA

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * return: alway 0
+ *
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 'a'; i <= 'z'; i++)
+		putchar (i);
+	for (j = 'A'; j <= 'Z'; j++)
+		putchar (j);
+	putchar('\n');
+	return (0);
+}
