b13e13faeac02f6207e0e0925c591b2eda1e76af
M-Abdulwaheed <muhd.abdulwaheed@gmail.com>
2023-08-15 21:50:42 -0700

    done

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *  _abs - a function that get the absolute value
+ *
+ *  @a: variable to be checked
+ *
+ *  Return: return value if greater that a
+ *
+ */
+
+int _abs(int a)
+{
+	if (a < 0)
+	{
+		return (-a);
+	}
+	else
+	{
+		return (a);
+	}
+}
+
+
