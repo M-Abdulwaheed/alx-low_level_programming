b3947f9e0e3281ecb718634f1a682aa40367c52f
M-Abdulwaheed <muhd.abdulwaheed@gmail.com>
2023-08-15 21:58:13 -0700

    3 colors

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_last_digit - function to print last digit of number
+ *
+ * Return: Always return 0 if successful
+ *
+ */
+
+int print_last_digit(int i)
+{
+	int j;
+
+	j =  i % 10;
+	if (i < 0)
+		j = -j;
+	_putchar(j + '0');
+	return (j);
+}
