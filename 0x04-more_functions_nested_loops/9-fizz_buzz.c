47dca8ee3eabb51a9ef0c5e82230e92f60191b29
M-Abdulwaheed <muhd.abdulwaheed@gmail.com>
2023-08-17 22:05:51 -0700

    Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,29 @@
+#include "stdio.h"
+/**
+ * main - function to solve fizzbuss
+ *
+ * @j: input to be checked
+ *
+ * Return: Always return void
+ */
+
+int main(void)
+{
+	int j;
+
+	for (j = 1 ; j <= 100 ; j++)
+	{
+		if (j % 3 == 0)
+			printf("%s", "Fizz");
+		else if (j % 5 == 0)
+			printf("%s", "Buzz");
+		else if ((j % 3 == 0) && (j % 5 == 0))
+			printf("%s", "FizzBuss");
+		else
+			printf("%d", j);
+		if (j != 100)
+			printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
