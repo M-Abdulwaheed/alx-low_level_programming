1c57077393420a8322a4a4e0d00ba2c341ffa6f5
M-Abdulwaheed <muhd.abdulwaheed@gmail.com>
2023-08-18 07:50:56 -0700

    Triangles

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,10 +8,25 @@
  */
 void print_triangle(int size)
 {
-	if (size < 1)
+	if (size <= 0)
+	{
 		_putchar('\n');
+	}
 	else
 	{
-		int i;
+		int i, j;
 
-		for (i = 1 ; i 
+		for (i = 1; i <= size; i++)
+		{
+			for (j = i; j < size; j++)
+			{
+				_putchar(' ');
+			}
+			for (j = 1; j <= i; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
