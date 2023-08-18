9a47c32da0bdd06fcdabf0165cb8d4b2527e5b7d
M-Abdulwaheed <muhd.abdulwaheed@gmail.com>
2023-08-18 06:25:56 -0700

    I feel like I am diagonally parked in a parallel universe

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_triangle - function to print triangle
+ *
+ * Return: Always return 0
+ *
+ */
+void print_triangle(int size)
+{
+	if (size < 1)
+		_putchar('\n');
+	else
+	{
+		int i;
+
+		for (i = 1 ; i 
--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,7 +10,7 @@
 
 void print_diagonal(int n)
 {
-	if (n < 1)
+	if (n <= 0)
 		_putchar('\n');
 	else
 	{
@@ -23,6 +23,5 @@ void print_diagonal(int n)
 			_putchar('\\');
 			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
